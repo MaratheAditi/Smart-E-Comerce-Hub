3e4878dae6ba6da7c348df2556e8d88bd6006b86
MaratheAditi <145250993+MaratheAditi@users.noreply.github.com>
2023-09-17 00:17:15 -0700

    Add files via upload

---
+++ END/database.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+#Open database
+conn = sqlite3.connect('database.db')
+
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password TEXT,
+		email TEXT,
+		firstName TEXT,
+		lastName TEXT
+		)''')
+
+conn.execute('''CREATE TABLE products
+		(productId INTEGER PRIMARY KEY,
+		name TEXT,
+		price REAL,
+		description TEXT,
+		image TEXT,
+		stock INTEGER,
+		categoryId INTEGER,
+		FOREIGN KEY(categoryId) REFERENCES categories(categoryId)
+		)''')
+
+conn.execute('''CREATE TABLE kart
+		(userId INTEGER,
+		productId INTEGER,
+		FOREIGN KEY(userId) REFERENCES users(userId),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
+
+conn.execute('''CREATE TABLE categories
+		(categoryId INTEGER PRIMARY KEY,
+		name TEXT
+		)''')
+
+
+conn.execute('''CREATE TABLE review 
+		(id INTEGER  PRIMARY KEY , 
+		name TEXT,
+		rating REAL,
+		content TEXT,
+		firstName TEXT,
+		productId INTEGER,
+		FOREIGN KEY(firstName) REFERENCES users(firstName),
+		FOREIGN KEY(productId) REFERENCES products(productId)
+		)''')
+
+conn.close()
+
